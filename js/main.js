efd95d6cb86ba5c62aa6f0d09454d484b4576e1f
ochukai <sex.xiaoyee@gmail.com>
2017-01-23 15:50:50 +0800

    Site updated: 2017-01-23 15:50:45

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(function () {
 
-  console.log('I am Oliver~');
-  
+	console.log('I am Oliver~');
+
+	var $oliBackTop = $("#oli-back-top");
+	// scroll to top
+	$oliBackTop.hide();
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$oliBackTop.fadeIn();
+		} else {
+			$oliBackTop.fadeOut();
+		}
+	});
+
+	// scroll body to 0px on click
+	$oliBackTop.on('click', function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500);
+		
+		return false;
+	});
+
+
 });
