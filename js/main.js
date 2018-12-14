6059cf961b569a20da8a2ec109e8d6ae437300a3
ochukai <sex.xiaoyee@gmail.com>
2018-12-14 15:05:51 +0800

    Site updated: 2018-12-14 15:05:27

--- js/main.js
+++ js/main.js
@@ -1,1 +1,132 @@
-$(function(){$(".post__main img").on("click",function(){var o=$(this);$.fancybox.open([{src:o.attr("src"),type:"image"}])}),$("[data-fancybox]").fancybox({image:{protect:!0}});var o=$("html"),e=!1;o.on("keydown",function(o){switch(o.which){case 74:e||(e=!0,requestAnimationFrame(function n(){var o=window.scrollY;window.scrollTo(0,o+15),e&&requestAnimationFrame(n)}));break;case 75:e||(e=!0,requestAnimationFrame(function a(){var o=window.scrollY;window.scrollTo(0,o-15),e&&requestAnimationFrame(a)}));break;case 191:break;case 84:window.scrollToTop(1);break;case 66:window.scrollToBottom();break;case 78:window.scrollPageDown(1);break;case 77:window.scrollPageUp(1)}}),o.on("keyup",function(o){e=!1});var n=["","                    .::::.            快捷键：","                  .::::::::.            j：下移","                 :::::::::::            k：上移","             ..:::::::::::'             t：移到最顶","           '::::::::::::'               b：移到最底","             .::::::::::                n：下移很多","        '::::::::::::::..               m：上移很多","             ..::::::::::::.","           ``::::::::::::::::","            ::::``:::::::::'        .:::.","           ::::'   ':::::'       .::::::::.","         .::::'      ::::     .:::::::'::::.","        .:::'       :::::  .::::::::'  ':::::.","       .::'        :::::::::::::::'      ':::::.","      .::'        :::::::::::::::'          ':::.","  ...:::          :::::::::::::'              ``::."," ```` ':.         '::::::::::'                  ::::..","                    ':::::'                    ':'````..",""];n.forEach(function(o){console.log("%c"+o,"color: #399c9c")}),$(".btn-reward").on("click",function(o){o.preventDefault();var e=$(".reward-wrapper");e.slideToggle()}),$("body").addClass("queue-in"),setTimeout(function(){$("body").css({opacity:1}).removeClass("queue-in")},500)});
+$(function () {
+
+	$('.post__main img').on('click', function () {
+		var $img = $(this);
+
+		$.fancybox.open([{
+			src: $img.attr('src'),
+			type: 'image'
+		}]);
+	});
+
+	$('[data-fancybox]').fancybox({
+		// closeClickOutside: false, 
+		image: {
+			protect: true
+		}
+	});
+
+	// key bind
+
+	// j  down
+	// k  top
+	// t  page top
+	// b  page bottom
+
+	// i  go index
+	var $body = $('html');
+
+	var isKeydown = false;
+	$body.on('keydown', function (e) {
+		// console.log(e.which, 'key down');
+
+		switch (e.which) {
+			case 74: // j down
+				if (!isKeydown) {
+					isKeydown = true;
+					requestAnimationFrame(function animate() {
+						var curTop = window.scrollY;
+						window.scrollTo(0, curTop + 15);
+
+						if (isKeydown) {
+							requestAnimationFrame(animate);
+						}
+					});
+				}
+
+				break;
+
+			case 75: // k up
+				if (!isKeydown) {
+					isKeydown = true;
+					requestAnimationFrame(function animate() {
+						var curTop = window.scrollY;
+						window.scrollTo(0, curTop - 15);
+
+						if (isKeydown) {
+							requestAnimationFrame(animate);
+						}
+					});
+				}
+
+				break;
+
+			case 191: // shift + / = ? show help modal
+				break;
+
+				// 16 shift
+			case 84: // t
+				window.scrollToTop(1);
+				break;
+
+			case 66: // b
+				window.scrollToBottom();
+				break;
+
+			case 78: // n half
+				window.scrollPageDown(1);
+				break;
+
+			case 77: // m
+				window.scrollPageUp(1);
+				break;
+		}
+
+	});
+
+	$body.on('keyup', function (e) {
+		isKeydown = false;
+	});
+
+	// print hint
+
+	var comments = [
+		'',
+		'                    .::::.            快捷键：',
+		'                  .::::::::.            j：下移',
+		'                 :::::::::::            k：上移',
+		"             ..:::::::::::'             t：移到最顶",
+		"           '::::::::::::'               b：移到最底",
+		'             .::::::::::                n：下移很多',
+		"        '::::::::::::::..               m：上移很多",
+		'             ..::::::::::::.',
+		'           ``::::::::::::::::',
+		"            ::::``:::::::::'        .:::.",
+		"           ::::'   ':::::'       .::::::::.",
+		"         .::::'      ::::     .:::::::'::::.",
+		"        .:::'       :::::  .::::::::'  ':::::.",
+		"       .::'        :::::::::::::::'      ':::::.",
+		"      .::'        :::::::::::::::'          ':::.",
+		"  ...:::          :::::::::::::'              ``::.",
+		" ```` ':.         '::::::::::'                  ::::..",
+		"                    ':::::'                    ':'````..",
+		''
+	];
+
+	comments.forEach(function (item) {
+		console.log('%c' + item, 'color: #399c9c');
+	});
+
+	$('.btn-reward').on('click', function (e) {
+		e.preventDefault();
+
+		var $reward = $('.reward-wrapper');
+		$reward.slideToggle();
+	});
+
+	$('body').addClass('queue-in');
+	setTimeout(function() {
+		$('body').css({ opacity: 1}).removeClass('queue-in');
+	}, 500);
+
+});
